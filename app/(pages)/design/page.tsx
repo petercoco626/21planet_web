2e9fa8882ef316a54005f0a3630c9219295dc483
planningo-peter <peter@planningo.io>
2024-02-17 01:39:27 +0900

    [style] input 일단 완성

--- app/(pages)/design/page.tsx
+++ app/(pages)/design/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+
 import { Button } from '@/components/base/button';
 import { GradientButton } from '@/components/base/gradient-button';
+import { Input } from '@/components/base/input';
+import { FormProvider, useForm } from 'react-hook-form';
 
 export default function DesignPage() {
+  const untitledFormMethods = useForm<{
+    foo: string;
+  }>({
+    mode: 'all',
+    defaultValues: {
+      foo: '',
+    },
+  });
+
   return (
     <div>
       <div className="space-y-3">
@@ -23,6 +36,26 @@ export default function DesignPage() {
         <Button variant="text" size="middle">
           button
         </Button>
+      </div>
+      <div>
+        <FormProvider {...untitledFormMethods}>
+          <Input
+            name="foo"
+            type="email"
+            placeholder={'foo'}
+            required={{
+              value: true,
+              message: 'Required',
+            }}
+            maxLength={{
+              value: 10,
+              message: 'Max length 10',
+            }}
+          />
+          <button type="submit" className="text-white">
+            submit
+          </button>
+        </FormProvider>
       </div>
     </div>
   );
--- components/base/input.tsx
+++ components/base/input.tsx
@@ -1,5 +1,11 @@
+import { useToggle } from '@/hooks/use-toggle';
 import clsx from 'clsx';
 import { RegisterOptions, useFormContext } from 'react-hook-form';
+import IcWarning from '@/assets/icon/ic-warning.svg';
+import IcEye from '@/assets/icon/ic-eye.svg';
+import IcEyeClosed from '@/assets/icon/ic-eye-closed.svg';
+
+type InputType = 'email' | 'password' | 'text';
 
 type InputProps<T> = {
   name: string;
@@ -8,50 +14,139 @@ type InputProps<T> = {
   accept?: string;
   placeholder?: T;
   defaultValue?: T;
-  type?: React.HTMLInputTypeAttribute;
+  type?: InputType;
   id?: string;
   multiple?: boolean;
+  errorMessage?: string;
 } & RegisterOptions;
+
+const textInputWrapperDefaultStyle =
+  'w-full h-12 px-4 box-border rounded-xl border-[1px] bg-gray-800 active:bg-gray-700 focus:bg-gray-700 placeholder:text-[rgba(255,255,255,0.3)]';
+
+const textInputDefaultStyle =
+  'text-white text-m_light bg-transparent outline-none';
 
 export function Input<T>(props: InputProps<T>) {
   const {
     name,
     labelName,
     placeholder,
-    type,
+    type = 'email',
     className,
     accept,
     id,
     multiple,
+    errorMessage,
     ...registerOptions
   } = props;
 
+  const errorStyle = {
+    label: errorMessage ? 'text-red-1' : 'text-white-0.5',
+    textInputBorder: errorMessage
+      ? 'border-red-1'
+      : 'border-gray-800 focus:border-gary-700',
+  };
+
   const { register, formState, getFieldState } = useFormContext();
+
+  const { toggle: isSecureText, handleToggle } = useToggle({
+    initialToggleState: true,
+  });
 
   const { error } = getFieldState(name, formState);
 
   return (
-    <div className={clsx('flex flex-col gap-0.5 items-start self-stretch')}>
-      <label
-        htmlFor={id}
-        className={'text-gray-600 text-s2'}
-      >
+    <div className={className}>
+      <label htmlFor={id} className={'text-s_medium mb-1 pl-2'}>
         {labelName}
       </label>
-      <input
-        id={id}
-        accept={accept}
-        type={type}
-        placeholder={typeof placeholder === 'string' ? placeholder : undefined}
-        className={clsx(
-          'outline-none px-3 py-[7px] text-white placeholder:text-gray-600',
-          'bg-gray-850 border',
-          'rounded w-full',
-          error ? 'border-system-red' : 'border-transparent focus:border-photio-green',
-        )}
-        {...register(name, registerOptions)}
-      />
-      <p className={'text-system-red text-b1'}>{error?.message || ''}</p>
+
+      {(() => {
+        if (type === 'email')
+          return (
+            <div
+              className={clsx(
+                textInputWrapperDefaultStyle,
+                errorStyle.textInputBorder
+              )}
+            >
+              <input
+                id={id}
+                accept={accept}
+                type={type}
+                placeholder={
+                  typeof placeholder === 'string' ? placeholder : undefined
+                }
+                className={clsx(
+                  'w-full h-full ',
+                  textInputDefaultStyle,
+                  error
+                    ? 'border-system-red'
+                    : 'border-transparent focus:border-photio-green'
+                )}
+                {...register(name, registerOptions)}
+              />
+            </div>
+          );
+
+        if (type === 'password')
+          return (
+            <div
+              className={clsx(
+                textInputWrapperDefaultStyle,
+                errorStyle.textInputBorder,
+                'flex flex-row justify-between items-center'
+              )}
+            >
+              <input
+                id={id}
+                accept={accept}
+                type={type}
+                placeholder={
+                  typeof placeholder === 'string' ? placeholder : undefined
+                }
+                className={clsx(textInputDefaultStyle, 'w-4/5')}
+                {...register(name, registerOptions)}
+              />
+              <button
+                onClick={handleToggle}
+                className="w-4 h-4 ml-2"
+                type="button"
+              >
+                {isSecureText ? <IcEyeClosed /> : <IcEye />}
+              </button>
+            </div>
+          );
+
+        if (type === 'text')
+          return (
+            <div
+              className={clsx(
+                textInputWrapperDefaultStyle,
+                errorStyle.textInputBorder
+              )}
+            >
+              <input
+                id={id}
+                accept={accept}
+                type={type}
+                placeholder={
+                  typeof placeholder === 'string' ? placeholder : undefined
+                }
+                className={clsx('w-full h-full', textInputDefaultStyle)}
+                {...register(name, registerOptions)}
+              />
+            </div>
+          );
+      })()}
+
+      {/** 여기 나중에 hook으로 처리해야함 */}
+      {errorMessage && (
+        <div className="pl-2 flex gap-x-1 flex-row items-center mt-[2px]">
+          <IcWarning />
+          <p className="text-xs_regular text-red-1">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }

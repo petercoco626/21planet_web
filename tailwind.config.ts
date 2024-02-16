14c74105291a0b29f3127c308070e505fd70c636
planningo-peter <peter@planningo.io>
2024-02-17 00:29:35 +0900

    [style] 안쓰는것들 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
         white: '#FFFFFF',
         'white-0.9': 'rgba(255, 255, 255, 0.90)',

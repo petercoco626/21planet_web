1db05d74e4d4da80188c8ad2c44c8ce88e092404
planningo-peter <peter@planningo.io>
2024-02-24 22:19:50 +0900

    [style] challenge-title style

--- components/challenge/comment/challenge-title.tsx
+++ components/challenge/comment/challenge-title.tsx
@@ -6,7 +6,8 @@ export function ChallengeTitle({ title }: ChallengeTitleProps) {
   return (
     <div className="flex justify-center">
       <div
-        className="mb-8 mt-4 border-[0.5px] border-white-0.5 text-white-0.9 relative flex items-center justify-center p-3 box-border break-keep rounded-xl"
+        className="mb-8 mt-4 border-[0.5px] border-white-0.15 text-white-0.9 relative flex items-center justify-center p-3 box-border break-keep rounded-xl backdrop-blur-lg
+        "
         style={{
           background:
             'linear-gradient(115deg,rgba(122,115,153,0.6) 0%, rgba(50,46,77,0.0) 98.94%)',

b6d4471235721c69fa022e65767c294c96bbe834
planningo-peter <peter@planningo.io>
2024-02-24 22:55:03 +0900

    [feat] 약관 테스트

--- components/setting/setting-options.tsx
+++ components/setting/setting-options.tsx
@@ -4,6 +4,7 @@ import IcCaretRight from '@/assets/icon/ic-caret-right.svg';
 import OptionListLayout from './option-list-layout';
 import { pathname } from '@/constants/path';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function SettingOptions() {
   const route = useRouter();
@@ -13,12 +14,17 @@ export default function SettingOptions() {
     options: [
       {
         content: (
-          <div className="w-full flex flex-row justify-between items-center">
+          <Link
+            href={
+              'https://zizzi-world.notion.site/3f2cc61446f44ff8b718862fd6352c61?pvs=4'
+            }
+            className="w-full flex flex-row justify-between items-center"
+          >
             <div className="text-m_light text-white">약관 및 동의 내용</div>
             <div className="w-8 h-8 flex flex-row justify-center items-center">
               <IcCaretRight className="stroke-gray-400" />
             </div>
-          </div>
+          </Link>
         ),
         onClick: () => {},
       },

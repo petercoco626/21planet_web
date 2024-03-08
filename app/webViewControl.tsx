18fc888237b6ceb422caee29415604de079c9bf7
planningo-peter <peter@planningo.io>
2024-03-08 21:05:15 +0900

    [feat] webview redirect logic

--- app/webViewControl.tsx
+++ app/webViewControl.tsx
@@ -1,38 +1,16 @@
 'use client';
 import { pathname } from '@/constants/path';
 import { useRouter } from 'next/navigation';
-import { use, useEffect } from 'react';
+import { useEffect } from 'react';
 
 export function WebViewControl() {
   const route = useRouter();
 
-  const isUIWebView = () => {
-    return navigator.userAgent
-      .toLowerCase()
-      .match(/\(ip.*applewebkit(?!.*(version|crios))/);
-  };
-
-  const onMessageEvent = (e: any) => {
-    const message = String(e.data);
-    if (message !== 'fromApp') {
+  useEffect(() => {
+    if (!window.isRNView) {
       route.replace(pathname.LOGIN);
     }
-  };
-
-  // useEffect(() => {
-  //   if (!isUIWebView()) {
-  //     route.replace(pathname.LOGIN);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   const receiver = isUIWebView() ? window : document;
-
-  //   receiver.addEventListener('message', onMessageEvent);
-  //   return () => {
-  //     receiver.removeEventListener('message', onMessageEvent);
-  //   };
-  // }, []);
+  }, []);
 
   return <></>;
 }

c8eed4fe7688def4fa8f9fd348f4912c002e9043
planningo-peter <peter@planningo.io>
2024-03-12 22:47:29 +0900

    [feat] kakao ad banner

--- app/(pages)/challenge/page.tsx
+++ app/(pages)/challenge/page.tsx
@@ -2,10 +2,12 @@ import { BottomNavigation } from '@/components/common/bottom-navigation';
 import { ChallengeList } from '@/components/challenge/challenge-list/challenge-list';
 import { Greeting } from '@/components/challenge/challenge-list/greeting';
 import { ChallengeListHeader } from '@/components/challenge/challenge-list/header';
+import { KakaoAdfit } from '@/components/third-party/kakao-adfit';
 
 export default function ChallengePage() {
   return (
     <main className="w-full h-full relative flex flex-col justify-between">
+      <KakaoAdfit />
       <div>
         <ChallengeListHeader />
         <Greeting />
